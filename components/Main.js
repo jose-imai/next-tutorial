3cfdc49944027be7c975c1c790f767f457251ef7
jose-imai <83395588+jose-imai@users.noreply.github.com>
2022-12-05 18:25:42 +0900

    前半のチュートリアル

---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Headline } from "../components/Headline";
+import Links from "../components/Links";
+import classes from "./Main.module.css";
+
+export function Main(props) {
+  return (
+    <main className={classes.main}>
+      <Headline page={props.page}>
+        <code className={classes.code}>pages/{props.page}.js</code>
+      </Headline>
+      <Links />
+    </main>
+  );
+}
