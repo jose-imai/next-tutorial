53092e5ead8305ada9e0c1bafe2062b0157ba7b7
jose-imai <83395588+jose-imai@users.noreply.github.com>
2022-12-07 11:52:05 +0900

    リファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,17 +1,43 @@
 import Image from "next/image";
 import classes from "./Links.module.css";
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Documentation →",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn →",
+    description: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://github.com/vercel/next.js/tree/canary/examples",
+    title: "Documentation →",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy →;",
+    description:
+      "Instantly deploy your Next.js site to a public URL with Vercel.",
+  },
+];
+
 export default function Links() {
   return (
     <div className={classes.grid}>
-      <a href="https://nextjs.org/docs" className={classes.card}>
-        <h2 className={classes.title}>Documentation &rarr;</h2>
-        <p className={classes.description}>
-          Find in-depth information about Next.js features and API.
-        </p>
-      </a>
+      {ITEMS.map((item) => {
+        return (
+          <a key={item.href} href={item.href} className={classes.card}>
+            <h2 className={classes.title}>{item.title}</h2>
+            <p className={classes.description}>{item.description}</p>
+          </a>
+        );
+      })}
 
-      <a href="https://nextjs.org/learn" className={classes.card}>
+      {/* <a href="https://nextjs.org/learn" className={classes.card}>
         <h2>Learn &rarr;</h2>
         <p>Learn about Next.js in an interactive course with quizzes!</p>
       </a>
@@ -32,7 +58,7 @@ export default function Links() {
       >
         <h2>Deploy &rarr;</h2>
         <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </a>
+      </a> */}
     </div>
   );
 }
